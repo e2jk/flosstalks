6096892bf449fc434f2c8faeeb29e89cbc2708b7
Emilien Klein <emilien@klein.st>
2012-06-21 19:14:08 +0200

    Added series list and series detail pages

--- flosstalks_app/urls.py
+++ flosstalks_app/urls.py
@@ -1,22 +1,22 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView
-from flosstalks_app.models import Project
+from flosstalks_app.models import Project, Series
 from flosstalks_app.views import TemplateView, ListView
 
 urlpatterns = patterns('',
-#    url(r'^$',
-#        ListView.as_view(
-#            queryset=Poll.objects.order_by('-pub_date')[:5],
-#            context_object_name='latest_poll_list',
-#            template_name='polls/index.html')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-#    url(r'^projects$', ListView.as_view(template_name='projects.html'), name='projects'),
     url(r'^projects$',
         ListView.as_view(
             model=Project,
             context_object_name='projects_list',
             template_name='projects_list.html'),
         name='projects'),
+    url(r'^series$',
+        ListView.as_view(
+            model=Series,
+            context_object_name='series_list',
+            template_name='series_list.html'),
+        name='series_list'),
     url(r'^about$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^p/(?P<pk>\d+)/$',
@@ -24,6 +24,11 @@ urlpatterns = patterns('',
             model=Project,
             template_name='project_detail.html'),
         name='project'),
+    url(r'^s/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Series,
+            template_name='series_detail.html'),
+        name='series'),
 #    url(r'^(?P<pk>\d+)/results/$',
 #        DetailView.as_view(
 #            model=Poll,
