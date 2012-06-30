a2e2d063623eca7c47116314087eb6b57d00bac1
Emilien Klein <emilien@klein.st>
2012-06-30 16:34:59 +0200

    Add the settings file to version control (SECRET_KEY is left out)

---
+++ flosstalks/DO_NOT_SHARE_please_rename.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file contains sensitive information and MUST NEVER be shared!
+# In particular, this file MUST NOT be put under version control.
+# Rename the example file to DO_NOT_SHARE.py and make the secret key unique
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'this is a secret key'
