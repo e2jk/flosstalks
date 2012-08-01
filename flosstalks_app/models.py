7c630d52c44558cc3d6439bd341968d1459d30f5
Emilien Klein <emilien@klein.st>
2012-08-02 01:51:07 +0200

    Show Ohloh widgets if the project's Ohloh ID is known

--- flosstalks_app/models.py
+++ flosstalks_app/models.py
@@ -25,7 +25,6 @@ class Project(models.Model):
     #TODO: Create a mechanism to retrieve a copy of the logo on the server
     # to prevent depending on external websites
     logo_url = models.URLField(null=True, blank=True)
-    #TODO: use ohloh_id
     ohloh_id = models.IntegerField("Ohloh ID", null=True, blank=True)
     STATUSES = (
         ('NW', 'New'),
