9772e7b3f2af8abbf88ce3110a22567eb349db35
Emilien Klein <emilien@klein.st>
2012-08-14 19:56:15 +0200

    Modularize retrievefromohloh to allow direct project querying on Ohloh ID next to searching on name

--- flosstalks_app/management/commands/retrievefromohloh.py
+++ flosstalks_app/management/commands/retrievefromohloh.py
@@ -29,48 +29,75 @@ class Command(BaseCommand):
             raise CommandError('Invalid argument')
         self.stdout.write("Retrieving project information from Ohloh for new projects:\n\n")
 
-        for p in Project.objects.filter(status="NW").exclude(skip_ohloh=True):
-            self.stdout.write("Project %d: %s" % (p.id, p.name))
+        # Query for new projects for which the Ohloh ID is known (set manually)
+        projects = Project.objects.filter(status="NW") \
+                                  .exclude(skip_ohloh=True) \
+                                  .exclude(ohloh_id=None)
+        self.stdout.write("Querying %d projects by Ohloh ID\n\n" % len(projects))
+        for p in projects:
+            self.stdout.write("Project %d: %s (Ohloh ID %d)\n" % (p.id, p.name, p.ohloh_id))
+            url = "https://www.ohloh.net/p/%(ohloh_id)d.xml?api_key=%(api_key)s" % {
+                    "ohloh_id": p.ohloh_id,
+                    "api_key": OHLOH_API_KEY,
+            }
+            data = self.get_result(url)
+            if data:
+                self.process_result(data, p, skip_name_check=True)
+
+        # Search all new projects on name
+        projects = Project.objects.filter(status="NW") \
+                                  .exclude(skip_ohloh=True)
+        self.stdout.write("\n\nSearching for %d projects by name\n\n" % len(projects))
+        for p in projects:
+            self.stdout.write("Project %d: %s\n" % (p.id, p.name))
             url = "http://www.ohloh.net/projects.xml?query=%(name)s&api_key=%(api_key)s" % {
                     "name": p.name,
                     "api_key": OHLOH_API_KEY,
             }
+            data = self.get_result(url)
+            if data:
+                self.process_result(data, p)
 
+    def get_result(self, url):
             try:
                 data = urllib2.urlopen(url).read()
             except urllib2.HTTPError, e:
                 self.stdout.write("\nHTTP error: %d\n\n" % e.code)
-                continue
+                return None
             except urllib2.URLError, e:
                 self.stdout.write("\nNetwork error: %s\n\n" % e.reason.args[1])
-                continue
+                return None
+            return data
 
-            doc = minidom.parseString(data)
-            if "success" == self.get_info_from_XML("status", doc):
-                if len(doc.getElementsByTagName("project")) > 0:
-                    # Only consider the first project
-                    self.item = doc.getElementsByTagName("project")[0]
-                    name = self.get_info_from_XML("name")
-                    if name.lower() == p.name.lower():
-                        # Only consider a match if the project names are identical
-                        self.stdout.write(" found on Ohloh!\n\n")
-                        # Update the project with the information from Ohloh!
-                        p.ohloh_id = self.get_info_from_XML("id")
-                        p.description = self.get_info_from_XML("description")
-                        p.description_source = self.get_info_from_XML("html_url")
-                        p.url = self.get_info_from_XML("homepage_url")
-                        p.logo_url = self.get_info_from_XML("medium_logo_url")
-                        # Mark the project as Pending. This allows the project to directly
-                        # be visible, but we can easily check if all seems fine.
-                        p.status = "PD"
-                        # Save the project
-                        p.save()
-                        continue
-                # If we get here, it means we couldn't fine a proper project on Ohloh
-                # This project must not be searched again on Ohloh, it will have to be done manually
-                self.stdout.write(" not found on Ohloh, skipping...\n")
-                p.skip_ohloh = True
-                p.save()
+    def process_result(self, data, p, skip_name_check=False):
+        doc = minidom.parseString(data)
+        if "success" == self.get_info_from_XML("status", doc):
+            if len(doc.getElementsByTagName("project")) > 0:
+                # Only consider the first project
+                self.item = doc.getElementsByTagName("project")[0]
+                name = self.get_info_from_XML("name")
+                if skip_name_check or name.lower() == p.name.lower():
+                    # Only consider a match if the project names are identical
+                    # or if we were doing a direct query on Ohloh ID
+                    self.stdout.write("    Found on Ohloh!\n\n")
+                    # Update the project with the information from Ohloh!
+                    p.ohloh_id = self.get_info_from_XML("id")
+                    p.description = self.get_info_from_XML("description")
+                    p.description_source = self.get_info_from_XML("html_url")
+                    p.url = self.get_info_from_XML("homepage_url")
+                    p.logo_url = self.get_info_from_XML("medium_logo_url")
+                    # Mark the project as Pending. This allows the project to directly
+                    # be visible, but we can easily check if all seems fine.
+                    p.status = "PD"
+                    # Save the project
+                    p.save()
+                    return True
+            # If we get here, it means we couldn't fine a proper project on Ohloh
+            # This project must not be searched again on Ohloh, it will have to be done manually
+            self.stdout.write("    Not found on Ohloh, skipping...\n")
+            p.skip_ohloh = True
+            p.save()
+            return False
 
     def get_info_from_XML(self, node, item=None):
         if not item:
