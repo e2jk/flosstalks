e51e67c03334d0f5a889497a27a2b781f20dc72e
Emilien Klein <emilien@klein.st>
2012-09-19 22:32:46 +0200

    Use i18n_patterns to prepend the current active language code to all url patterns. This will allow for better caching.

--- flosstalks/urls.py
+++ flosstalks/urls.py
@@ -16,19 +16,74 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with FLOSS Talks.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls.i18n import i18n_patterns
+from django.views.decorators.cache import cache_page
 from django.contrib import admin
+from flosstalks_app.models import Project, Series, Resource
+from flosstalks_app.views import TemplateView, ListView, DetailView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    # Enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
 
-    url(r'^', include('flosstalks_app.urls')),
+flosstalks_patterns = patterns('',
+    url(r'^$',
+        cache_page(300)(# Cache the home page only for 5 minutes
+            TemplateView.as_view(template_name='index.html')
+        ),
+        name='home'),
+    url(r'^projects$',
+        ListView.as_view(
+            queryset=Project.objects.exclude(status="HD")\
+                                    .exclude(status="NW")\
+                                    .order_by('name'),
+            paginate_by=20,
+            context_object_name='projects_list',
+            template_name='projects_list.html'),
+        name='projects'),
+    url(r'^series$',
+        ListView.as_view(
+            model=Series,
+            context_object_name='series_list',
+            template_name='series_list.html'),
+        name='series_list'),
+    url(r'^about$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^p/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Project,
+            template_name='project_detail.html'),
+        name='project'),
+    url(r'^p/(?P<pk>\d+)/add-resource$',
+        'flosstalks_app.views.project_add_resource',
+        name='project_add_resource'),
+    url(r'^p/(?P<pk>\d+)/resource-added$',
+        DetailView.as_view(
+            model=Project,
+            template_name='project_resource_added.html'),
+        name='project_resource_added'),
+    url(r'^s/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Series,
+            template_name='series_detail.html'),
+        name='series'),
+    url(r'^r/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Resource,
+            template_name='resource_detail.html'),
+        name='resource'),
+    url(r'^search$', 'flosstalks_app.views.search'),
+    url(r'^search-values.json$', 'flosstalks_app.views.get_search_values'),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^(?P<requested_value>.+)$', 'flosstalks_app.views.nice_url'),
+)
+
+urlpatterns += i18n_patterns('',
+    url(r'^', include(flosstalks_patterns, namespace='flosstalks')),
 )
